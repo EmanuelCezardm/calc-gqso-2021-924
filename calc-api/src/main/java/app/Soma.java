43a7a253aca41358b2cc53781fda4cc0c6fcc5e5
EmanuelCezardm <emanuelcezar2003@gmail.com>
2021-08-18 14:26:40 -0300

    Aprimoramento na mensagem de erro

--- calc-api/src/main/java/app/Soma.java
+++ calc-api/src/main/java/app/Soma.java
@@ -11,14 +11,36 @@ public class Soma {
     public String rotaSoma(@PathParam String op1, @PathParam String op2){
 
         try {
+            
             double num1 = Double.parseDouble(op1);
             double num2 = Double.parseDouble(op2);
             double soma = num1 + num2;
             
             return Double.toString(soma);
+            
         } catch (NumberFormatException nfe) {
+            
+            if(verificacaoDeNumero(op1) && verificacaoDeNumero(op2) == false){
+
+                throw new BadRequestException(String.format("Parâmetro inválido: \\%s ", op1));
+
+            }else if(verificacaoDeNumero(op1) == false && verificacaoDeNumero(op2)){
+
+                throw new BadRequestException(String.format("Parâmetro inválido: \\%s", op2));
+
+            }
+
             throw new BadRequestException(String.format("Parâmetros inválidos: \\%s\\%s", op1, op2));
         }
         
+    }
+
+    private Boolean verificacaoDeNumero(String num){
+        try {
+            Double.parseDouble(num);
+            return false;
+        } catch (NumberFormatException nfe) {
+            return true;
+        }
     }
 }
